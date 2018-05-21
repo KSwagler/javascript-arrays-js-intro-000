475729585c7b259d7a93c7a8ef1a6f0a7de6f235
Kelsey Swagler <kelsey.swagler@gmail.com>
2018-05-21 18:45:08 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -24,5 +24,6 @@ function destructivelyRemoveElementFromBeginningOfArray(array) {
   return array
 }
 function removeElementFromBeginningOfArray(array) {
-   return array.slice (1)
-}
+   return array.slice(1)
+ }
+ 
